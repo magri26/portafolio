ff01c43f8f8f500ba778b131219a8e553221fad0
dsolutions dsolutions <dsolutions@MacBook-Air-de-dsolutions.local>
2026-01-29 12:18:53 -0300

    se cambian textos, se agrega seccion reclutadores

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const toggle = document.getElementById('themeToggle');
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('light');
+  toggle.textContent = document.body.classList.contains('light') ? '🌞' : '🌙';
+});
+
+const roleToggle = document.getElementById('roleToggle');
+const roleText = document.getElementById('roleText');
+
+let recruiter = true;
+
+roleToggle.addEventListener('click', () => {
+  recruiter = !recruiter;
+
+  roleText.textContent = recruiter
+    ? 'Desarrollador Full-Stack & Mobile enfocado en lógica de negocio y problemas complejos'
+    : 'Desarrollo soluciones web y móviles enfocadas en eficiencia, escalabilidad y resultados';
+
+  roleToggle.textContent = recruiter ? 'Reclutadores' : 'Clientes';
+});
